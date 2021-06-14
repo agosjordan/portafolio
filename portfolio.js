a447d5e5a8433a76c7c59511ecd09298034791a6
Agos <agos.jordan@gmail.com>
2021-06-14 18:16:40 -0300

    Iniciamos (no funciona active)

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+var navLink = document.getElementsByClassName("nav-link")
+
+window.onscroll = function() {stickyNavbar()};
+navBar()
+
+function stickyNavbar() {
+  if (window.pageYOffset >= sticky) {
+     navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+function navBar(){
+  navLink.map(item => {
+    item.addEventListener("click", function(){
+      this.classList.toggle("active")  
+    })
+  });
+}
